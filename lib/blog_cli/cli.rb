bbc7600d9112a217d881b9c9a15a3c3d1a959341
aviflombaum <avi@flombaum.com>
2016-03-19 17:59:40 -0400

    Sort of cool

--- lib/blog_cli/cli.rb
+++ lib/blog_cli/cli.rb
@@ -1,14 +1,23 @@
 class BlogCLI::CLI
-  attr_reader :last_input
+  attr_reader :last_input, :current_user
 
   def call
     puts "Welcome to your CLI Blog!"
+    login
     menu
+  end
+
+  def login
+    puts "Please enter your name to login:"
+    @current_user = BlogCLI::Author.find_or_create_by(:name => user_input)
+    puts "You're now logged in as: #{current_user.name}"
   end
 
   def menu
     puts "What would you like to do?"
     puts "1. Write a post"
+    puts "2. List your posts"
+    puts "3. List all posts"
     main_menu_loop
   end
 
@@ -17,6 +26,10 @@ class BlogCLI::CLI
       case last_input.to_i
       when 1
         post_new
+        break
+      when 2
+        post_index
+        break
       else
         menu
         break
@@ -24,8 +37,54 @@ class BlogCLI::CLI
     end
   end
 
+  def post_index
+    puts "--- Posts by you, #{current_user.name} ---"
+    # How to print out each post by this user with an ID and a Title
+    current_user.posts.each do |post|
+      puts "#{post.id}. #{post.title}"
+    end
+
+    puts "Enter the Post ID you'd like to see or edit or go back to the main menu:"
+    if user_input.to_i > 0
+      post_show
+    else
+      menu
+    end
+  end
+
+  def post_show
+    puts "Loading Post #{last_input}..."
+    # When we load this post, it 100% belongs to the current_user
+    # begin
+    #   post = current_user.posts.find(last_input)
+    if post = current_user.posts.find_by(:id => last_input)
+      puts "--- #{post.id} --- #{post.title}"
+      puts
+      puts post.content
+    else
+    # rescue ActiveRecord::RecordNotFound
+      puts "Can't find a post with ID #{last_input} for you..."
+    end
+    menu
+  end
+
   def post_new
     params = {}
+    # When we're creating a Blog Post, assuming the author already exists
+    # How many SQL Statements should fire?
+    # 1 SQL Statement - an "INSERT INTO posts"
+
+    # post = BlogCLI::Post.new(params)
+    # How can I associate this blog post with author?
+    # in 2 ways.
+
+    # 1. Let's manually assign the current_user as the author of this post
+    # post.author = current_user
+
+    # Why this is bad.
+    # Will load all of an authors posts into memory
+    # even though we don't need them!!!
+    # current_user.posts << post
 
     puts "Please enter the title of your new post:"
     params[:title] = user_input
@@ -33,9 +92,12 @@ class BlogCLI::CLI
     puts "Please enter the content of your new post:"
     params[:content] = user_input
 
-    post = BlogCLI::Post.new(params)
-
-    post.save
+    # 2. Let's actually instantiate the post already associated with the
+    # current_user
+    post = current_user.posts.build(params)
+    post.save # Insert the Post with the author_id FK
+    puts "Saved Post ##{post.id}..."
+    menu
   end
 
   private
--- lib/blog_cli/post.rb
+++ lib/blog_cli/post.rb
@@ -1,5 +1,6 @@
 class BlogCLI::Post < ActiveRecord::Base
-  belongs_to :author
+  belongs_to :author # What does this code do?
+  # 1. We can create data or we can define methods.
   belongs_to :category
-  
+
 end
